78ba014489af6986fbc391a92161dcf36e820f41
Mario Jurado <64239144+Mario0125@users.noreply.github.com>
2022-08-26 18:36:10 -0500

    métodos get y set clase vuelos

--- src/main/java/com/vuelafacil/entidades/Vuelos.java
+++ src/main/java/com/vuelafacil/entidades/Vuelos.java
@@ -9,5 +9,63 @@ package com.vuelafacil.entidades;
  * @author mariojurado
  */
 public class Vuelos {
+    private String namCiudadOrigen;
+    private String namCiudadDestino;
+    private String nombrePiloto;
+    private String  nombreCopiloto;
+    private String  [] azafatas;
+    private String  [] pasajeros;
+    
+public void setNamCiudadOrigen(String nombreCiudad){
+    this.namCiudadOrigen = namCiudadOrigen;
+    }
+
+public void setNamCiudadDestino(String namCiudadDestino){
+    this.namCiudadDestino = namCiudadDestino;
+    }
+
+public void setNombrePiloto(String nombrePiloto){
+    this.nombrePiloto = nombrePiloto;
+    }
+
+public void setNombreCopiloto(String nombreCopiloto){
+    this.nombreCopiloto = nombreCopiloto;
+    }
+
+public void setAzafatas(String [] azafatas){
+    this.azafatas = azafatas;
+    }
+
+public void setPasajeros(String [] pasajeros){
+    this.pasajeros = pasajeros;
+    }
+
+public String getNamCiudadOrigen(){
+    return this.namCiudadOrigen;
+    }
+
+public String getNamCiudadDestino(){
+    return this.namCiudadDestino;
+    }
+
+public String getNombrePiloto(){
+    return this.nombrePiloto;
+    }
+
+public String getNombreCopiloto(){
+    return this.nombreCopiloto;
+    }
+
+public String [] getAzafatas(){
+    return this.azafatas;
+    }
+
+public String [] getPasajeros(){
+    return this.pasajeros;
+    }
+
+
+
+
     
 }

379962c2e88e34e311801e37d7cbaf5edad132cb
Mario Jurado <64239144+Mario0125@users.noreply.github.com>
2022-08-26 15:33:01 -0500

    métodos get y set clase ciudades

--- src/main/java/com/vuelafacil/entidades/Ciudad.java
+++ src/main/java/com/vuelafacil/entidades/Ciudad.java
@@ -9,5 +9,53 @@ package com.vuelafacil.entidades;
  * @author mariojurado
  */
 public class Ciudad {
+    private String nombreCiudad;
+    private String nombreAeropuerto;
+    private String codigoAeropuerto;
+    private boolean estadoAeropuerto;
+    
+    public Ciudad (String nombreCiudad, String nombreAeropuerto, String codigoAeropuerto, boolean estadoAeropuerto){
+        this.nombreCiudad = nombreCiudad;
+        this.nombreAeropuerto = nombreAeropuerto;
+        this.codigoAeropuerto = codigoAeropuerto;
+        this.estadoAeropuerto = estadoAeropuerto;
+        }
+    //  metodos set
+    
+    public void setNombreCiudad(String nombreCiudad){
+        this.nombreCiudad = nombreCiudad;
+    }
+    
+    public void setNombreAeropuerto(String nombreAeropuerto){
+        this.nombreAeropuerto = nombreAeropuerto;
+    }
+    
+    public void setCodigoAeropuerto(String codigoAeropuerto){
+        this.codigoAeropuerto = codigoAeropuerto;
+    }
+
+    public void setEstadoAeropuerto(boolean estadoAeropuerto){
+        this.estadoAeropuerto = estadoAeropuerto;
+    }
+    
+    // métodos get
+    
+    public String getNombreCiudad(){
+        return this.nombreCiudad;
+    }
+    
+    public String getNombrAeropuerto(){
+        return this.nombreAeropuerto;
+    }
+    
+    public String getCodigoAeropuerto(){
+       return this.codigoAeropuerto;
+    }
+    
+    
+
+        
+    
+    
     
 }

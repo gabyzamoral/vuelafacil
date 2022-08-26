39c2f3368ac6873332d8a35962291dcab93c2348
Mario Jurado <64239144+Mario0125@users.noreply.github.com>
2022-08-26 16:23:42 -0500

    métodos get y set clase rutas

--- src/main/java/com/vuelafacil/entidades/Rutas.java
+++ src/main/java/com/vuelafacil/entidades/Rutas.java
@@ -9,5 +9,61 @@ package com.vuelafacil.entidades;
  * @author mariojurado
  */
 public class Rutas {
+    private String nombreRuta;
+    private String codigoRuta;
+    private int frecuenciaRuta;
+    private int horaSalida;
+    private int horaLlegada;
+
+public Rutas (String nombreRuta, String codigoRuta, int frecuenciaRuta, int horaSalida, int horaLlegada){
+    this.nombreRuta = nombreRuta;
+    this.codigoRuta = codigoRuta;
+    this.frecuenciaRuta = frecuenciaRuta;
+    this.horaSalida = horaSalida;
+    this.horaLlegada = horaLlegada;
     
 }
+
+public void setNombreRuta(String nombreRuta){
+    this.nombreRuta = nombreRuta;
+}
+
+public void setCodigoRuta(String codigoRuta){
+    this.codigoRuta = codigoRuta;
+}
+
+public void setFrecuenciaRuta(int frecuenciaRuta){
+    this.frecuenciaRuta = frecuenciaRuta;
+}
+
+public void setHoraSalida(int horaSalida){
+    this.horaSalida = horaSalida;
+}
+
+public void setHoraLlegada(int horaLlegada){
+    this.horaLlegada = horaLlegada;
+}
+
+public String getNombreRuta(){
+    return this.nombreRuta;
+}
+
+public String CodigoRuta(){
+    return this.codigoRuta;
+}
+
+public int getFrecuenciaRuta(){
+    return this.frecuenciaRuta;
+}
+
+public int getHoraSalida(){
+    return this.horaSalida;
+}
+
+public int getHoraLlegada(){
+    return this.horaLlegada;
+}
+
+}
+
+

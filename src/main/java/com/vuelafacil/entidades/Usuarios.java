eb7bbb3ccf0269958c95368657e60c5a5e661e78
Mario Jurado <64239144+Mario0125@users.noreply.github.com>
2022-08-26 20:49:33 -0500

    métodos get y set clase Usuarios

--- src/main/java/com/vuelafacil/entidades/Usuarios.java
+++ src/main/java/com/vuelafacil/entidades/Usuarios.java
@@ -9,5 +9,50 @@ package com.vuelafacil.entidades;
  * @author mariojurado
  */
 public class Usuarios {
+    private String nombreAdministrador;
+    private String nombreUsuario;
+    private String contraseñaAdm;
+    private String contraseñaUsuario;
+
+        public  Usuarios(String nombreAdministrador, String nombreUsuario, String contraseñaAdm, String contraseñaUsuario){
+            this.nombreAdministrador = nombreAdministrador;
+            this.nombreUsuario = nombreUsuario;
+            this.contraseñaAdm = contraseñaAdm;
+            this.contraseñaUsuario = contraseñaUsuario;
+
+        }
     
+        public void setNombreAdministrador(String nombreAdministrador){
+            this.nombreAdministrador = nombreAdministrador;
+        }
+
+        public void setNombreUsuario(String nombreUsuario){
+            this.nombreUsuario = nombreUsuario;
+        }
+
+        public void setContraseñaAdm(String contraseñaAdm){
+            this.contraseñaAdm = contraseñaAdm;
+        }
+
+        public void setContraseñaUsuario(String contraseñaUsuario){
+            this.contraseñaUsuario = contraseñaUsuario;
+        }
+
+
+        public String getNombreAdministrador(String nombreAdministrador){
+            return this.nombreAdministrador;
+        }
+
+        public String getNombreUsuario(String nombreUsuario){
+            return this.nombreUsuario;
+        }
+
+        public String getContraseñaAdm(String contraseñaAdm){
+            return this.contraseñaAdm;
+        }
+        
+        public String getContraseñaUsuario(String contraseñaUsuario){
+            return this.contraseñaUsuario;
+        }
+        
 }

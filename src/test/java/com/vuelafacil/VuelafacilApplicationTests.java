31bd7d1be44e0cd8365af13d91ffd312e5914cc3
Mario Jurado <64239144+Mario0125@users.noreply.github.com>
2022-09-01 23:15:13 -0500

    Prueba satisfactoria de la clase Ciudad

--- src/test/java/com/vuelafacil/VuelafacilApplicationTests.java
+++ src/test/java/com/vuelafacil/VuelafacilApplicationTests.java
@@ -3,6 +3,7 @@ package com.vuelafacil;
 import com.vuelafacil.entidades.Ciudad;
 import com.vuelafacil.servicios.CiudadServicio;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,13 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VuelafacilApplicationTests {
 
-        @Autowired
-        private CiudadServicio servicio;
-	@Test
-	void probarSiSeCreaUnaNuevaCategoria() {
-            Ciudad c = new Ciudad("Bucaramanga","PaloNegro", "BUC", true );
-            Ciudad guardado = servicio.crearNuevaCiudad(c);
-            Assertions.assertTrue(guardado.getIdentificador() >0, "Eror, no se guardo el nuevoo objeto");
-	}
-
+    @Autowired
+    private CiudadServicio servicio;
+    
+    
+    @Test
+    @Disabled     
+    void probarSiSeCreaUnaNuevaCiudad(){
+        Ciudad c = new Ciudad("Bucaramanga", "PaloNegro", "BUC", true);
+        Ciudad guardado = servicio.crearNuevaCiudad(c);
+        Assertions.assertTrue(guardado.getIdentificador() > 0 , "Error no se guardo el nuevo objeto");
+        
+    }
 }

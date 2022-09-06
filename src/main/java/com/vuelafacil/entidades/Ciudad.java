2485000b5d125b6d1e07b59469ed8c0f746f5437
Gabriela Zamora López <111836303+gabyzamoral@users.noreply.github.com>
2022-09-05 20:31:53 -0500

    Create Ciudad.java

---
+++ src/main/java/com/vuelafacil/entidades/Ciudad.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vuelafacil.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author mariojurado
+ */
+@Entity(name ="ciudad")
+public class Ciudad {
+    
+    @Id
+    @Column(name = "ciudid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int identificador;
+    
+    @Column(name = "ciudnombre", nullable = false, length = 80)
+    private String nombreCiudad;
+    
+    @Column(name = "ciudnombreaeropuerto", nullable = false, length = 100)    
+    private String nombreAeropuerto;
+
+    @Column(name = "ciudcodigoaeropuerto", nullable = false, length = 3)    
+    private String codigoAeropuerto;
+
+    @Column(name = "ciudhabilitado", columnDefinition = "TinyINT")    
+    private boolean estadoAeropuerto;
+    
+    
+    public Ciudad(){
+        
+    }
+    
+    public Ciudad (int identificador, String nombreCiudad, String nombreAeropuerto, String codigoAeropuerto, boolean estadoAeropuerto){
+        this.identificador = identificador;
+        this.nombreCiudad = nombreCiudad;
+        this.nombreAeropuerto = nombreAeropuerto;
+        this.codigoAeropuerto = codigoAeropuerto;
+        this.estadoAeropuerto = estadoAeropuerto;
+    }
+
+    public Ciudad (String nombreCiudad, String nombreAeropuerto, String codigoAeropuerto, boolean estadoAeropuerto){
+        this.nombreCiudad = nombreCiudad;
+        this.nombreAeropuerto = nombreAeropuerto;
+        this.codigoAeropuerto = codigoAeropuerto;
+        this.estadoAeropuerto = estadoAeropuerto;
+    }
+    //  metodos set
+    
+    public void setIdentificador(int identificador){
+        this.identificador = identificador;
+    }
+    
+    public void setNombreCiudad(String nombreCiudad){
+        this.nombreCiudad = nombreCiudad;
+    }
+    
+    public void setNombreAeropuerto(String nombreAeropuerto){
+        this.nombreAeropuerto = nombreAeropuerto;
+    }
+    
+    public void setCodigoAeropuerto(String codigoAeropuerto){
+        this.codigoAeropuerto = codigoAeropuerto;
+    }
+
+    public void setEstadoAeropuerto(boolean estadoAeropuerto){
+        this.estadoAeropuerto = estadoAeropuerto;
+    }
+    
+    // métodos get
+    
+    public int getIdentificador(){
+        return this.identificador;
+    }
+    
+    
+    public String getNombreCiudad(){
+        return this.nombreCiudad;
+    }
+    
+    public String getNombreAeropuerto(){
+        return this.nombreAeropuerto;
+    }
+    
+    public String getCodigoAeropuerto(){
+       return this.codigoAeropuerto;
+    }
+    
+    
+    
+}

eef73b7392ee602baadc8baf125fc6e29f2c0c9d
Gabriela Zamora López <111836303+gabyzamoral@users.noreply.github.com>
2022-09-13 21:44:02 -0500

    Cambio Dashboard

--- src/main/java/com/vuelafacil/controladores/ControladorPrincipal.java
+++ src/main/java/com/vuelafacil/controladores/ControladorPrincipal.java
@@ -29,11 +29,11 @@ public class ControladorPrincipal
         return "login";
     } 
 
-    @GetMapping("/index")//Ramon aqui le cambie la ruta para que me llegue al Index y no 
+    @GetMapping("/dashboard")//Ramon aqui le cambie la ruta para que me llegue al Index y no 
                          //   a la dashboard
     public  String dashboard()
     {
-        return "index";
+        return "dashboard";
     }  
 
     @GetMapping("/salir")
